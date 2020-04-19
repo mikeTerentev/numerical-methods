13329dd31c1adba12620b18c118b2ab1ad1d8e4c
Valery Teplyakov <Valeriy.Teplyakov@jetbrains.com>
2020-04-20 01:36:17 +0300

    A little fix

--- task1/newton_to_gif.R
+++ task1/newton_to_gif.R
@@ -6,7 +6,7 @@ f <- function(x) x^7 - 5 * x^6 - 12 * x^5 + sqrt(2) * x^2 - 4
 
 boundaries <- tibble(left = c(-1.9, -1.5, 6.4), right = c(-1.5, -0.5, 7.1))
 
-for (root_num in 2:2) {
+for (root_num in 1:3) {
   animatedData <- read.csv(file.path("task1", paste0("out", root_num, ".csv"))) %>% mutate(Legend = "Tangent")
   xs <- seq(boundaries[["left"]][root_num], boundaries[["right"]][root_num], by = 0.00001)
   staticData <- tibble(x = xs, y = f(x), Legend = "Function")
